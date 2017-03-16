b9a0560ffe515161be541c34744893983a582331
Kanishk <Kanishk>
2017-03-16 12:18:57 +0530

    Minor improvements.

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ dispatcher = updater.dispatcher
 # Real stuff
 def start(bot, update):
 	#bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-	bot.sendMessage(chat_id=update.message.chat_id, text="Hi!")
+	bot.sendMessage(chat_id=update.message.chat_id, text="Hi! I'm a telegram Bot for Filebot!")
 
 def chatid(bot, update):
 	#bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
@@ -30,7 +30,7 @@ def chatid(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Your Chat ID is: "+ "*" + chatid + "*", parse_mode='markdown')
 
 def intro(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text= "I'm a telegram Bot for Filebot!")
+	bot.sendMessage(chat_id=update.message.chat_id, text="I'm a telegram Bot for Filebot!")
 
 def unknown(bot, update):
 	#bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
@@ -41,14 +41,14 @@ start_handler = CommandHandler('start', start)
 chatid_handler = CommandHandler('chatid', chatid)
 intro_handler = MessageHandler('Who are you?', intro)
 unknown_command = MessageHandler(Filters.command, unknown)
-#unknown_message = MessageHandler(Filters.messages, unknown)
+unknown_message = MessageHandler(Filters.text, unknown)
 
 # Dispatchers
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(chatid_handler)
 dispatcher.add_handler(intro_handler)
 dispatcher.add_handler(unknown_command)
-#dispatcher.add_handler(unknown_message)
+dispatcher.add_handler(unknown_message)
 
 updater.start_polling()
 updater.idle()
