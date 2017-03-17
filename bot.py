fb50310357602dcbe58dfa7b32fefc5666e8a2fb
Kanishk <singh.kanishk07@gmail.com>
2017-03-17 20:25:56 +0530

    this doesn't work

--- bot.py
+++ bot.py
@@ -20,6 +20,11 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 dispatcher = updater.dispatcher
 
 # Real stuff
+def intro(bot, update):
+	bot.sendMessage(chat_id=update.message.chat_id, text="I'm a telegram Bot for Filebot!")
+intro_handler = MessageHandler('Who are you?', intro)
+dispatcher.add_handler(intro_handler)
+
 def start(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Hi! I'm a telegram Bot for Filebot!")
 start_handler = CommandHandler('start', start)
@@ -31,10 +36,7 @@ def chatid(bot, update):
 chatid_handler = CommandHandler('chatid', chatid)
 dispatcher.add_handler(chatid_handler)
 
-def intro(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="I'm a telegram Bot for Filebot!")
-intro_handler = MessageHandler('Who are you?', intro)
-dispatcher.add_handler(intro_handler)
+
 
 def unknown(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, I didn't get that.")
