a5c6e9ae7f721fb0fea17ec1ae2e42981ff053c3
Kanishk <singh.kanishk07@gmail.com>
2017-03-17 21:21:10 +0530

    this works now

--- bot.py
+++ bot.py
@@ -23,29 +23,32 @@ dispatcher = updater.dispatcher
 def start(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Hi! I'm a telegram Bot for Filebot!")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
 def intro(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="I'm a telegram Bot for Filebot!")
-
-intro_handler = RegexHandler('Who are you?', intro)
-dispatcher.add_handler(intro_handler)
 
 def chatid(bot, update):
 	chatid = str(update.message.chat_id)
 	bot.sendMessage(chat_id=update.message.chat_id, text="Your Chat ID is: "+ "*" + chatid + "*", parse_mode='markdown')
 
-chatid_handler = CommandHandler('chatid', chatid)
-dispatcher.add_handler(chatid_handler)
-
 def unknown(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, I didn't get that.")
 
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
+
 # Handlers
+start_handler = CommandHandler('start', start)
+intro_handler = RegexHandler('Who are you?', intro)
+chatid_handler = CommandHandler('chatid', chatid)
+unknown_handler = MessageHandler(Filters.command, unknown)
+unknown_message = MessageHandler(Filters.text, unknown)
+
+
 # Dispatchers
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(intro_handler)
+dispatcher.add_handler(chatid_handler)
+dispatcher.add_handler(unknown_handler)
+dispatcher.add_handler(unknown_message)
+
 
 #dp.add_handler(RegexHandler(r"$What's your name\?^", callback)) FROM jh0ker
 
